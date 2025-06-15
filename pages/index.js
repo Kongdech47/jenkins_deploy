f59f402ecb7bffb41560f45da2a7b6e0daa17032
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2025-06-15 10:22:33 +0700

    Add initial project files for Next.js application with Docker support

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Demo Web App 1</h1>
+      <p>Welcome! This is a simple Next.js demo running in Docker.</p>
+    </div>
+  );
+}
