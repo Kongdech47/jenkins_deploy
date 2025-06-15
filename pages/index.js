5b3972c09504871d5c8059cbfeb342ac3722f239
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2025-06-15 11:56:40 +0700

    Update title in Home component to reflect version change

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Demo Web App 1</h1>
+      <h1>Demo Web App 2</h1>
       <p>Welcome! This is a simple Next.js demo running in Docker.</p>
     </div>
   );
