2e367d7e8e69304773d8d2b7c4af8b3c43fb4be0
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2025-06-15 21:15:35 +0700

    Fix syntax error in Home component's JSX structure

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}
       <h1>Demo Web App 2</h1>
       <p>Welcome! This is a simple Next.js demo running in Docker.</p>
     </div>
